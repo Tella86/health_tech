8a6a801738b14132e8c94bfae3937d047badbc56
Tella87 <tellazone0@gmail.com>
2024-07-18 19:09:04 +0300

    modify

--- hms/admin/include/dbcontroller.php
+++ hms/admin/include/dbcontroller.php
@@ -1,8 +1,8 @@
 <?php
 $DB_host = "localhost";
 $DB_user = "root";
-$DB_pass = "";
-$DB_name = "ingram";
+$DB_pass = "Elon2508/*-";
+$DB_name = "healthtech";
 try
 {
  $DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
