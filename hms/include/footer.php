de059c241cd02a7b04776ab3028a334a718ad1be
Tella87 <tellazone0@gmail.com>
2024-07-18 16:58:03 +0300

    creating footer

--- hms/include/footer.php
+++ hms/include/footer.php
@@ -1,10 +1,12 @@
-<footer>
-				<div class="footer-inner">
-					<div class="pull-left">
-						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> HMS</span>. <span>All rights reserved</span>
-					</div>
-					<div class="pull-right">
-						<span class="go-top"><i class="ti-angle-up"></i></span>
-					</div>
-				</div>
-			</footer>
+<center><footer class="Copyright">
+     Ezems Tech Developers, 1333 Kilifi, County, Kenya<br>
+    <p>Copyright &copy; 2024 Ezems Tech Developera | All Rights Reserved</p>
+    <p><b>
+                    Follow us on 
+                    <a href='https://www.facebook.com/Ezemstech' target='_blank'>Facebook</a>, 
+                    <a href='https://www.twitter.com/ezems' target='_blank'>Twitter</a>, 
+                    <a href='https://www.instagram.com/ezems' target='_blank'>Instagram</a>
+                    <a href='https://www.linkedin.com/in/ziroh-katana-24b23ba9/' target='_blank'>LinkedIn</a>, and 
+                    <a href='https://github.com/Tella86/Tella86' target='_blank'>GitHub</a>
+                </p>
+</footer></center>
