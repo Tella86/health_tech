9766c2957701e191fe343cdff4d90b471a9e9eed
Tella87 <tellazone0@gmail.com>
2024-07-18 23:32:14 +0300

    footer update

--- hms/user-login.php
+++ hms/user-login.php
@@ -99,9 +99,19 @@ exit();
 						</fieldset>
 					</form>
 
-					<div class="copyright">
-						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> HMS</span>. <span>All rights reserved</span>
-					</div>
+					
+					<center><footer class="Copyright">
+						Ezems Tech Developers, 1333 Kilifi, County, Kenya<br>
+					   <p>Copyright &copy; 2024 Ezems Tech Developera | All Rights Reserved</p>
+					   <p>
+									   Follow us on 
+									   <a href='https://www.facebook.com/Ezemstech' target='_blank'>Facebook</a>, 
+									   <a href='https://www.twitter.com/ezems' target='_blank'>Twitter</a>, 
+									   <a href='https://www.instagram.com/ezems' target='_blank'>Instagram</a>
+									   <a href='https://www.linkedin.com/in/ziroh-katana-24b23ba9/' target='_blank'>LinkedIn</a>, and 
+									   <a href='https://github.com/Tella86/Tella86' target='_blank'>GitHub</a>
+								   </p>
+				   </footer></center>
 			
 				</div>
 
