69f94d46310858888d87c820f04156fa144b534f
Tella87 <tellazone0@gmail.com>
2024-07-20 18:27:15 +0300

    css and update code

--- hms/admin/dashboard.php
+++ hms/admin/dashboard.php
@@ -10,7 +10,9 @@ check_login();
 
 <head>
     <title>Admin | Dashboard</title>
-    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+    <link
+        href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic"
+        rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
@@ -23,14 +25,15 @@ check_login();
     <link href="vendor/bootstrap-timepicker/bootstrap-timepicker.min.css" rel="stylesheet" media="screen">
     <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="assets/css/plugins.css">
+    <link rel="stylesheet" href="assets/css/display.css">
     <link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <style>
-        /* Add necessary styles here */
-        #dashboard-content {
-            padding: 20px;
-        }
+    /* Add necessary styles here */
+    #dashboard-content {
+        padding: 20px;
+    }
     </style>
 </head>
 
@@ -163,70 +166,139 @@ check_login();
                                     </div>
                                 </div>
                             </div>
-                            <?php require 'display.html'; ?>
+                            <div class="col-sm-4">
+                                <div class="panel panel-white no-radius text-center">
+                                    <div class="panel-body">
+                                        <span class="fa-stack fa-2x">
+                                            <!-- <i class="fas fa-envelope fa-1x" style="color: #007bff;"></i> -->
+                                        </span>
+                                        <div class="notification-icon">
+                                            <i class="fas fa-envelope" style="color: #007bff;"></i>
+                                            <div class="badge" id="notification-badge">0</div>
+                                        </div>
+
+                                        <!-- Notification List -->
+                                        <div class="notification-list" id="notification-list">
+                                            <ul id="notification-items"></ul>
+                                        </div>
+
+                                        <h2 class="StepTitle">Messages</h2>
+
+                                    </div>
+                                </div>
+                                <!-- end: BASIC EXAMPLE -->
+                            </div>
                         </div>
                     </div>
-                    <!-- end: BASIC EXAMPLE -->
+                    <!-- start: FOOTER -->
+                    <?php include('include/footer.php'); ?>
+                    <!-- end: FOOTER -->
+                    <!-- start: SETTINGS -->
+                    <?php include('include/setting.php'); ?>
+                    <!-- end: SETTINGS -->
                 </div>
-            </div>
-        </div>
-        <!-- start: FOOTER -->
-        <?php include('include/footer.php'); ?>
-        <!-- end: FOOTER -->
-        <!-- start: SETTINGS -->
-        <?php include('include/setting.php'); ?>
-        <!-- end: SETTINGS -->
-    </div>
-    <!-- start: MAIN JAVASCRIPTS -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="vendor/modernizr/modernizr.js"></script>
-    <script src="vendor/jquery-cookie/jquery.cookie.js"></script>
-    <script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-    <script src="vendor/switchery/switchery.min.js"></script>
-    <!-- end: MAIN JAVASCRIPTS -->
-    <!-- start: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
-    <script src="vendor/maskedinput/jquery.maskedinput.min.js"></script>
-    <script src="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
-    <script src="vendor/autosize/autosize.min.js"></script>
-    <script src="vendor/selectFx/classie.js"></script>
-    <script src="vendor/selectFx/selectFx.js"></script>
-    <script src="vendor/select2/select2.min.js"></script>
-    <script src="vendor/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
-    <script src="vendor/bootstrap-timepicker/bootstrap-timepicker.min.js"></script>
-    <!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
-    <!-- start: CLIP-TWO JAVASCRIPTS -->
-    <script src="assets/js/main.js"></script>
-    <script src="assets/js/form-elements.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js"></script>
-    <script>
-        $(document).ready(function () {
-            $('#create-zoom-link').click(function (event) {
-                event.preventDefault(); // Prevent the default link action
-                $('#dashboard-content').load('../../hms/zoom.html');
-            });
-            
-            Main.init();
-            FormElements.init();
-            
-            // Calendar
-            $('#calendar').fullCalendar({
-                height: 'auto',
-                contentHeight: 300,
-                events: 'include/fetch-meetings.php',
-                eventRender: function (event, element) {
-                    element.find('.fc-title').append("<br/><a href='" + event.link + "' target='_blank'>Join Meeting</a>");
-                    element.on('click', function () {
-                        window.open(event.link, '_blank');
+                <!-- start: MAIN JAVASCRIPTS -->
+                <script src="vendor/jquery/jquery.min.js"></script>
+                <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+                <script src="vendor/modernizr/modernizr.js"></script>
+                <script src="vendor/jquery-cookie/jquery.cookie.js"></script>
+                <script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+                <script src="vendor/switchery/switchery.min.js"></script>
+                <!-- end: MAIN JAVASCRIPTS -->
+                <!-- start: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+                <script src="vendor/maskedinput/jquery.maskedinput.min.js"></script>
+                <script src="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
+                <script src="vendor/autosize/autosize.min.js"></script>
+                <script src="vendor/selectFx/classie.js"></script>
+                <script src="vendor/selectFx/selectFx.js"></script>
+                <script src="vendor/select2/select2.min.js"></script>
+                <script src="vendor/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+                <script src="vendor/bootstrap-timepicker/bootstrap-timepicker.min.js"></script>
+                <!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+                <!-- start: CLIP-TWO JAVASCRIPTS -->
+                <script src="assets/js/main.js"></script>
+                <script src="assets/js/form-elements.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js"></script>
+                <link rel="stylesheet"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+                <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+                <script>
+                $(document).ready(function() {
+                    $('#create-zoom-link').click(function(event) {
+                        event.preventDefault(); // Prevent the default link action
+                        $('#dashboard-content').load('../../hms/zoom.html');
                     });
-                }
-            });
-        });
-    </script>
-    <!-- end: JavaScript Event Handlers for this page -->
-    <!-- end: CLIP-TWO JAVASCRIPTS -->
+
+                    Main.init();
+                    FormElements.init();
+
+                    // Calendar
+                    $('#calendar').fullCalendar({
+                        height: 'auto',
+                        contentHeight: 300,
+                        events: 'include/fetch-meetings.php',
+                        eventRender: function(event, element) {
+                            element.find('.fc-title').append("<br/><a href='" + event.link +
+                                "' target='_blank'>Join Meeting</a>");
+                            element.on('click', function() {
+                                window.open(event.link, '_blank');
+                            });
+                        }
+                    });
+                });
+
+                $(document).ready(function() {
+                    // Fetch notifications
+                    function fetchNotifications() {
+                        $.get('fetch_notifications.php', function(data) {
+                            let notifications = JSON.parse(data);
+                            if (notifications.length > 0) {
+                                $('#notification-badge').text(notifications.length).show();
+                                $('#notification-items').empty();
+                                notifications.forEach(function(notification) {
+                                    $('#notification-items').append(`
+                                <li>
+                                    <a href="view_message.php?id=${notification.id}">
+                                        ${notification.notification}
+                                    </a>
+                                </li>
+                            `);
+                                });
+                            } else {
+                                $('#notification-badge').hide();
+                                $('#notification-items').empty();
+                            }
+                        });
+                    }
+
+                    // Check for new notifications every 5 seconds
+                    setInterval(fetchNotifications, 5000);
+
+                    // Initial fetch
+                    fetchNotifications();
+
+                    // Toggle notification list visibility
+                    $('.notification-icon').click(function() {
+                        $('#notification-list').toggle();
+                    });
+
+                    // Hide notification list when clicking outside
+                    $(document).click(function(event) {
+                        if (!$(event.target).closest('.notification-icon, #notification-list')
+                            .length) {
+                            $('#notification-list').hide();
+                        }
+                    });
+                });
+                </script>
+                <!-- end: JavaScript Event Handlers for this page -->
+                <!-- end: CLIP-TWO JAVASCRIPTS -->
+</body>
+
+</html>
+
 </body>
 
 </html>
--- hms/admin/view_message.php
+++ hms/admin/view_message.php
@@ -31,93 +31,197 @@ if (isset($_GET['id'])) {
 $con->close();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>View Message</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f2f2f2;
-            margin: 0;
-            padding: 20px;
+                <!DOCTYPE html>
+                <html lang="en">
+
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Dashboard</title>
+                    <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                        background-color: #f2f2f2;
+                        margin: 0;
+                        padding: 20px;
+                    }
+
+                    .chat-container {
+                        max-width: 600px;
+                        margin: 0 auto;
+                        background-color: #fff;
+                        padding: 20px;
+                        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                        border-radius: 10px;
+                    }
+
+                    .message-container {
+                        padding: 10px 0;
+                    }
+
+                    .message {
+                        background-color: #e5e5ea;
+                        padding: 10px 20px;
+                        border-radius: 20px;
+                        margin: 10px 0;
+                        max-width: 80%;
+                    }
+
+                    .message.outgoing {
+                        background-color: #007bff;
+                        color: white;
+                        align-self: flex-end;
+                    }
+
+                    .message.incoming {
+                        background-color: #e5e5ea;
+                        align-self: flex-start;
+                    }
+
+                    .reply-form {
+                        display: flex;
+                        flex-direction: column;
+                        margin-top: 20px;
+                    }
+
+                    .reply-form textarea {
+                        width: 100%;
+                        padding: 10px;
+                        margin-bottom: 10px;
+                        border: 1px solid #ccc;
+                        border-radius: 10px;
+                    }
+
+                    .reply-form button {
+                        padding: 10px 20px;
+                        background-color: #007bff;
+                        color: white;
+                        border: none;
+                        border-radius: 10px;
+                        cursor: pointer;
+                    }
+
+                    .reply-form button:hover {
+                        background-color: #0056b3;
+                    }
+
+                    .modal {
+                        display: none;
+                        position: fixed;
+                        z-index: 1;
+                        left: 0;
+                        top: 0;
+                        width: 100%;
+                        height: 100%;
+                        overflow: auto;
+                        background-color: rgb(0, 0, 0);
+                        background-color: rgba(0, 0, 0, 0.4);
+                        padding-top: 60px;
+                    }
+
+                    .modal-content {
+                        background-color: #fefefe;
+                        margin: 5% auto;
+                        padding: 20px;
+                        border: 1px solid #888;
+                        width: 80%;
+                    }
+
+                    .close {
+                        color: #aaa;
+                        float: right;
+                        font-size: 28px;
+                        font-weight: bold;
+                    }
+
+                    .close:hover,
+                    .close:focus {
+                        color: black;
+                        text-decoration: none;
+                        cursor: pointer;
+                    }
+                    </style>
+                </head>
+
+                <body>
+                    <div class="chat-container">
+                        <button id="viewMessageBtn">View Message</button>
+                    </div>
+
+                    <div id="messageModal" class="modal">
+                        <div class="modal-content">
+                            <span class="close">&times;</span>
+                            <div class="message-container">
+                                <?php if ($message): ?>
+                                <div class="message incoming">
+                                    <strong>From: <?php echo htmlspecialchars($message['name']); ?></strong><br>
+                                    <p><?php echo nl2br(htmlspecialchars($message['message'])); ?></p>
+                                </div>
+                                <?php else: ?>
+                                <p>No message found.</p>
+                                <?php endif; ?>
+                            </div>
+
+                            <?php if ($message): ?>
+                            <div class="reply-form">
+                                <h2>Reply to this Message</h2>
+                                <form id="replyForm" action="send_sms.php" method="post">
+                                    <textarea name="reply" rows="5" required
+                                        placeholder="Type your reply here..."></textarea>
+                                    <input type="hidden" name="mobileno"
+                                        value="<?php echo htmlspecialchars($message['mobileno']); ?>">
+                                    <input type="hidden" name="id"
+                                        value="<?php echo htmlspecialchars($message['id']); ?>">
+                                    <button type="submit">Send Reply</button>
+                                </form>
+                            </div>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+
+                    <script>
+    var modal = document.getElementById("messageModal");
+    var btn = document.getElementById("viewMessageBtn");
+    var span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
         }
-        .chat-container {
-            max-width: 600px;
-            margin: 0 auto;
-            background-color: #fff;
-            padding: 20px;
-            box-shadow: 0 0 10px rgba(0,0,0,0.1);
-            border-radius: 10px;
-        }
-        .message-container {
-            padding: 10px 0;
-        }
-        .message {
-            background-color: #e5e5ea;
-            padding: 10px 20px;
-            border-radius: 20px;
-            margin: 10px 0;
-            max-width: 80%;
-        }
-        .message.outgoing {
-            background-color: #007bff;
-            color: white;
-            align-self: flex-end;
-        }
-        .message.incoming {
-            background-color: #e5e5ea;
-            align-self: flex-start;
-        }
-        .reply-form {
-            display: flex;
-            flex-direction: column;
-            margin-top: 20px;
-        }
-        .reply-form textarea {
-            width: 100%;
-            padding: 10px;
-            margin-bottom: 10px;
-            border: 1px solid #ccc;
-            border-radius: 10px;
-        }
-        .reply-form button {
-            padding: 10px 20px;
-            background-color: #007bff;
-            color: white;
-            border: none;
-            border-radius: 10px;
-            cursor: pointer;
-        }
-        .reply-form button:hover {
-            background-color: #0056b3;
-        }
-    </style>
-</head>
-<body>
-    <div class="chat-container">
-        <?php if ($message): ?>
-            <div class="message-container">
-                <div class="message incoming">
-                    <strong>From: <?php echo htmlspecialchars($message['name']); ?></strong><br>
-                    <p><?php echo nl2br(htmlspecialchars($message['message'])); ?></p>
-                </div>
-            </div>
-            
-            <div class="reply-form">
-                <h2>Reply to this Message</h2>
-                <form action="send_sms.php" method="post">
-                    <textarea name="reply" rows="5" required placeholder="Type your reply here..."></textarea>
-                    <input type="hidden" name="mobileno" value="<?php echo htmlspecialchars($message['mobileno']); ?>">
-                    <input type="hidden" name="id" value="<?php echo htmlspecialchars($message['id']); ?>">
-                    <button type="submit">Send Reply</button>
-                </form>
-            </div>
-        <?php else: ?>
-            <p>No message found.</p>
-        <?php endif; ?>
-    </div>
-</body>
-</html>
+    }
+
+    document.getElementById('replyForm').onsubmit = function(event) {
+        event.preventDefault(); // prevent form submission
+
+        // Disable the submit button and show loading message
+        var submitButton = document.querySelector('button[type="submit"]');
+        submitButton.disabled = true;
+        submitButton.innerText = "Sending...";
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "send_sms.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    alert("Reply sent successfully");
+                    modal.style.display = "none";
+                    window.location.href = "dashboard.php";
+                } else {
+                    alert("Error sending reply. Please try again.");
+                    submitButton.disabled = false;
+                    submitButton.innerText = "Send Reply";
+                }
+            }
+        };
+        xhr.send(new URLSearchParams(new FormData(this)).toString());
+    };
+</script>
